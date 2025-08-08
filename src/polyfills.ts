86f19085e1240f8cd3e355ecd09c10c76d56194d
ostolaza <i.ostolaza@gmail.com>
2025-08-08 17:21:05 +0900

    Added polyfills

---
+++ src/polyfills.ts
@@ -0,0 +1,8 @@
+// This file is intentionally empty for modern browsers. Add polyfills if supporting older browsers like IE.
+// Reference: https://angular.dev/guide/browser-support
+
+(window as any).global = window;
+(window as any).process = {
+  env: { DEBUG: undefined },
+};
+
