047711c3d04d05e5ea705ae66cddd6e6333b8924
ostolaza <i.ostolaza@gmail.com>
2025-09-01 21:39:32 +0900

    Amplify storage edit

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,9 +1,10 @@
+
 import { defineStorage } from '@aws-amplify/backend';
 
 export const storage = defineStorage({
   name: 'profileImages',
   access: (allow) => ({
-    'profile/{identityId}/*': [
+    'profile/{entity_id}/*': [ 
       allow.authenticated.to(['read', 'write', 'delete']),
       allow.entity('identity').to(['write', 'delete'])
     ]
