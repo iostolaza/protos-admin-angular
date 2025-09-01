cbabc166cfa2475faf1e247b1b821008ef115ad5
ostolaza <i.ostolaza@gmail.com>
2025-09-01 17:31:05 +0900

    Remove Amplify files and dependencies

--- amplify/auth/post-confirmation/resource.ts
+++
@@ -1,6 +0,0 @@
-// amplify/auth/post-confirmation/resource.ts
-import { defineFunction } from '@aws-amplify/backend';
-
-export const postConfirmation = defineFunction({
-  name: 'post-confirmation',
-});
